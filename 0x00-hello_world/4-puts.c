80c0ec76b0454b720fba61032183e4dbff7eeba4
Musakalamz <ogunsolumusa@gmail.com>
2023-07-13 13:19:09 -0700

    Hello, puts
     prints exactly "Programming is like building a multilingual puzzle, followed by a new line.
    
    Use the function puts
    
    You are not allowed to use printf
    
    Your program should end with the value 0

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,19 @@
+/*
+ * File: 4-puts.c
+ * Auth; Musa_kalam A Ogunsolu
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "Programming is like building a multilingual puzzle, followed by a new line
+ *
+ * return - always 0.
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
