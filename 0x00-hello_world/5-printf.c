c688bf30d1b11f8ffae8abedad245830859f4e26
Musakalamz <ogunsolumusa@gmail.com>
2023-07-13 14:08:56 -0700

    Hello, printf
    prints exactly with proper grammar, but the outcome is a piece of art,, followed by a new line.
    
    Use the function printf
    
    You are not allowed to use the function puts
    
    Your program should return 0
    
    Your program should compile without warning when using the -Wall gcc option

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,19 @@
+/*
+ * File: 5-printf.
+ * Auth: Musa_kalamz A Ogunsolu
+ */
+
+#include <stdio.h>
+
+/*
+ * main - prints with proper grammar, but the outcome is a piece of ar
+ *
+ * return - Always 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+
+	return (0);
+}
