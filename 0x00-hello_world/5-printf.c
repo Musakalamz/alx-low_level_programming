b542bbf15ab13b2e6f2b2e360f23c3260eea9b90
Musakalamz <ogunsolumusa@gmail.com>
2023-07-13 15:13:37 -0700

    printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,19 +1,20 @@
 /*
- * File: 5-printf.
+ * File: 5-printf.c
  * Auth: Musa_kalamz A Ogunsolu
  */
 
 #include <stdio.h>
 
-/*
- * main - prints with proper grammar, but the outcome is a piece of ar
- *
- * return - Always 0
+/**
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * 
+ * Return - 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art\n");
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	
 	return (0);
 }
