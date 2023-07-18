2944968ccc4fd7c2c09f3c21b5b56db6d3bf4b72
Musakalamz <ogunsolumusa@gmail.com>
2023-07-18 08:37:40 -0700

    10x a-z

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -16,7 +16,7 @@ void print_alphabet_x10(void)
 	char letter;
 	int digit;
 
-	for (digit = '1'; digit <= '10'; digit++)
+	for (digit = 1; digit <= 10; digit++)
 	{
 		for (letter = 'a'; letter <= 'z'; letter++)
 		{
