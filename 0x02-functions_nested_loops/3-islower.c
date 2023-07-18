35b68309f4abb6e0427f45cd6749fb12b8193cc6
Musakalamz <ogunsolumusa@gmail.com>
2023-07-18 09:59:47 -0700

    commenting base on betty styling

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,9 +6,9 @@
 #include "main.h"
 
 /**
- *_islower - Checks if a character is lowercase
- * @c - character to be checkex
- * 
+ * _islower - Checks if a character is lowercase.
+ * @c: The character to be checked.
+ *
  * Return: 1 if character is lowercase, 0 otherwise.
  */
 
