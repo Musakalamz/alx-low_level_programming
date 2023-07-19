936a8027c813a4b5c148a17a7a79525baa059d1e
Musakalamz <ogunsolumusa@gmail.com>
2023-07-19 15:09:37 -0700

    header file

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,14 +3,6 @@
 
 #include <stdio.h>
 
-/* 
-  * File: main.h 
-  * Auth: Musa_kalamz A Ogunsolu 
-  * Desc: Header file containing declarations for all functions used in the 0x03-debugging directory. 
-  */
-
 void positive_or_negative(int i);
-
-
 
 #endif /* MAIN_H */
