e9ddd711a0a943f2d8019826697d61907236d624
Musakalamz <ogunsolumusa@gmail.com>
2023-07-19 15:00:30 -0700

    Multiple main

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/* 
+  * File: main.h 
+  * Auth: Musa_kalamz A Ogunsolu 
+  * Desc: Header file containing declarations for all functions used in the 0x03-debugging directory. 
+  */
+
+void positive_or_negative(int i);
+
+
+
+#endif /* MAIN_H */
