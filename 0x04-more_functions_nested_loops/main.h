a481d69885ee72434e6e4139df7035b0a2c50f26
Musakalamz <ogunsolumusa@gmail.com>
2023-07-20 07:27:40 -0700

    Header file

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,11 +1,12 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* 
-  * File: main.h 
-  * Auth: Musa_kalamz A Ogunsolu 
-  * Desc: Header file containing declarations for all functions used in the 0x04-more_functions_nested_loops directory. 
-  */
+/*
+ * File: holberton.h
+ * Auth: Musa_kalamz A Ogunsolu
+ * Desc: Header file containing prototypes for all functions
+ *       used in the 0x04-more_functions_nested_loops directory.
+ */
 
 int _putchar(char c);
 int _isupper(int c);
@@ -14,10 +15,10 @@ int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
-void print_line(int n);;
+void print_line(int n);
 void print_diagonal(int n);
-
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
 
-#endif /* MAIN_H */
+#endif
