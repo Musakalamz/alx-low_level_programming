256f13cfd64168854c984164f11c66d3009edb3c
Musakalamz <ogunsolumusa@gmail.com>
2023-07-20 03:25:52 -0700

    isupper
    checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* 
+  * File: main.h 
+  * Auth: Musa_kalamz A Ogunsolu 
+  * Desc: Header file containing declarations for all functions used in the 0x04-more_functions_nested_loops directory. 
+  */
+
+int _putchar(char c);
+int _isupper(int c);
+
+#endif /* MAIN_H */
