0c3c07eddb09bdc63e805177280a83f47b412e5f
Musakalamz <ogunsolumusa@gmail.com>
2023-07-24 16:25:01 -0700

    This report, by its very length, defends itself against the risk of being read
    a function that returns the length of a string.

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,23 +1,27 @@
-/*
- * File: 2-strlen.c
- * Auth: Musa_kalamz A Ogunsolu
- */
+/* 
+  * File: 2-strlen.c 
+  * Auth: Musa_kalamz A Ogunsolu 
+  */
 
 #include "main.h"
 
 /**
- * _strlen - Returns the length of a string.
- * @s: The string to get the length of.
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
  *
- * Return: The length of @s.
+ * Return: the length of the string
  */
 
 int _strlen(char *s)
 {
-	int length = 0;
+	int length;
 
-	while (*s++ != '\0')
-		length÷÷;
+	length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
 
 	return (length);
 }
