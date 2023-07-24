52d35c3af471d68b8a4855fa7a2464296b353e94
Musakalamz <ogunsolumusa@gmail.com>
2023-07-24 08:42:10 -0700

    98 Battery st
    a function that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: Muss_kalamz A Ogunsolu
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - Takes a pointet to an int and update
+ *		the value its points tp to 98.
+ * @n: The pointer to an int.
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
