7816f316b2748c6fe27da89941297559460e42cb
Musakalamz <ogunsolumusa@gmail.com>
2023-08-07 16:26:43 -0700

    Float like a butterfly, sting like a bee
    function that creates an array of chars, and initializes it with a specific char.

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,36 @@
+/*
+ * file: 0-create_array.c
+ * Auth: Musa_kalamz A Ogunsolu
+ */
+
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - creates an array of chars, an
+ *		  initializes it with a specific char
+ * @size: The size of the array to be initialized.
+ * @c: The specific character to initiallize the array with.
+ *
+ * Return: If size == 0 or the function fails - NULL.
+ *	   Otherwise, a pointer to the array.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *array;
+	int index;
+
+	if (size == 0)
+		return (NULL);
+
+	array = (char *) malloc(sizeof(char) * size);
+
+	if (array == 0)
+		return (NULL);
+
+	for (index = 0; index < size; index++)
+		array[index] = c;
+
+	return (array);
+}
