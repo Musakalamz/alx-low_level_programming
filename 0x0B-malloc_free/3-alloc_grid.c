6ccb6fccaa13d4d505532c42de331b93c0552fdc
Musakalamz <ogunsolumusa@gmail.com>
2023-08-08 10:32:27 -0700

    If you even dream of beating me you'd better wake up and apologize
    a function that returns a pointer to a 2 dimensional array of integers.

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -24,7 +24,7 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	twoD = (int *)malloc(sizeof(int *) * height);
+	twoD = malloc(sizeof(int *) * height);
 
 	if (twoD == NULL)
 		return (NULL);
