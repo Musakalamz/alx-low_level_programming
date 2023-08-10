69182c6c6158168bc329714944f2a4940cdba68b
Musakalamz <ogunsolumusa@gmail.com>
2023-08-10 15:38:47 -0700

    We must accept finite disappointment, but never lose infinite hope
    a program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -3,81 +3,127 @@
  * Auth: Musa_kalamz A Ogunsolu
  */
 
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
 
-int find_len(int *arr);
-void *_calloc(unsigned int nmemb, unsigned int size);
-int main(int *num1, int *num2);
-
-/**
- * find_len - finds length of array
- * @arr: array to find length of
- * Return: length of the array
- */
-
-int find_len(int *arr)
-{
-	int len;
-
-	for (len = 0; arr[len]; len++)
-		;
-	return (len);
-}
+int is_digit(char *s);
+int _strlen(char *s);
+void errors(void);
+int main(int argc, char *argv[]);
 
 /**
- * _calloc - allocates memory for an array of size bites
- * @nmemb: what to fill array with
- * @size: type of byte to allocate for memory
- * Return: returns pointer to allocated memory otherwise NULL on fail
+ * is_digit - checks if a string contains a non-digit char
+ * @s: string to be evaluated
+ *
+ * Return: 0 if a non-digit is found, 1 otherwise
  */
 
-void *_calloc(unsigned int nmemb, unsigned int size)
+int is_digit(char *s)
 {
-	char *array;
-	unsigned int index;
+	int index = 0;
 
-	if (nmemb == 0 || size == 0)
-		return (NULL);
-
-	array = malloc(nmemb * size);
-
-	if (array == NULL)
-		return (NULL);
-
-	for (index = 0; index < nmemb * size; index++)
-		array[index] = 0;
-
-	return ((void *)array);
-}
-
-/**
- * main - multiplies two postivie numbers
- * @num1: first number to multiply by
- * @num2: second number to multiply by
- * Return: result followed by new line
- */
-
-int main(int *num1, int *num2)
-{
-	int indx1, indx2, carry, len1, len2, *total;
-
-	len1 = find_len(num1);
-	len2 = find_len(num2);
-
-	if (len1 > len2)
-		total = _calloc(len1 * 2, sizeof(int));
-
-	else
-		total = _calloc(len2 * 2, sizeof(int));
-
-	if (!total)
+	while (s[index])
 	{
-		free(arr);
-		return (NULL);
+		return (0);
+
+		index++;
 	}
 
-	indx1 = 0;
-	indx2 = 0;
+	return (1ndex);
+}
 
+/**
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+	int index = 0;
+
+	while (s[index] != '\0')
+	{
+		index++;
+	}
+
+	return (index);
+}
+
+/**
+ * errors - handles errors for main
+ */
+
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
+ * main - multiplies two positive numbers
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	char *s1, *s2;
+	int len1, len2, len, index, carry, digit1, digit2, *result, a = 0;
+
+	s1 = argv[1], s2 = argv[2];
+
+	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+		errors();
+
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
+	len = len1 + len2 + 1;
+
+	result = malloc(sizeof(int) * len);
+
+	if (!result)
+		return (1);
+
+	for (index = 0; index <= len1 + len2; index++)
+		result[index] = 0;
+
+	for (len1 = len1 - 1; len1 >= 0; len1--)
+	{
+		digit1 = s1[len1] - '0';
+		carry = 0;
+
+		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		{
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
+			result[len1 + len2 + 1] = carry % 10;
+			carry /= 10;
+		}
+
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
+
+	for (index = 0; index < len - 1; index++)
+	{
+		if (result[index])
+			a = 1;
+
+		if (a)
+			_putchar(result[index] + '0');
+	}
+
+	if (!a)
+		_putchar('0');
+
+	_putchar('\n');
+
+	free(result);
+
+	return (0);
 }
