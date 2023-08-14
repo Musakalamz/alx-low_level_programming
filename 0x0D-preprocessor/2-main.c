918bc7aa1ab47da9ff5c7853fb0eaa51f1272cd1
Musakalamz <ogunsolumusa@gmail.com>
2023-08-14 01:45:35 -0700

    File name
    a program that prints the name of the file it was compiled from, followed by a new line.

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,20 @@
+/*
+ * File: 2-main.c
+ * Auth: Musa_kalamz A Ogunsolu
+ */
+
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file it wa
+ *	  compiled from, followed by a new line
+ *
+ * Return: Alays 0.
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
