eb627a39a5818336e808e50f6af3705779ab6c0b
Musakalamz <ogunsolumusa@gmail.com>
2023-08-14 04:53:48 -0700

    SUM
     a function-like macro SUM(x, y) that computes the sum of the numbers x and y.

--- 0x0D-preprocessor/4-sum.h
+++ 0x0D-preprocessor/4-sum.h
@@ -4,7 +4,8 @@
 /*
  * File: 4-sum.h
  * Auth: Musa_kalamz A Ogunsolu
- * Desc: Header file that computes the sum of the numbers x and y
+ * Desc: Header file that defines a function-like macro SUM(x, y)
+ *       that computes the sum of the numbers x and y.
  */
 
 #define SUM(x, y) ((x) + (y))
