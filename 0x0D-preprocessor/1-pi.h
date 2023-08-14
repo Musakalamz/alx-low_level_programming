0e43ee00897fc12cfadbc670df61fb14417181d6
Musakalamz <ogunsolumusa@gmail.com>
2023-08-14 01:25:19 -0700

    Pi
    Create a header file that defines a macro named PI as an abbreviation for the token 3.14159265359.

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef PI_H
+#define PI_H
+
+/*
+ * File: 1-pi.h
+ * Auth: Musa_kalamz A Ogunsolu
+ * Desc: Header file that defines a macro named PI as an
+ *	 abbreviation for the token 3.1415926535
+ */
+
+#define PI 3.14159265359
+
+#endif
