5c0488ebf0a2cdd847f78567bbf3ee5dcac31e7d
Musakalamz <ogunsolumusa@gmail.com>
2023-08-16 11:02:01 -0700

    Header file containing all structures and prototypes used by the 3-main.c program.

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -2,22 +2,21 @@
 #define CALC_H
 
 /*
- * File: calc.h
+ * File: 3-calc.h
  * Auth: Musa_kalamz A Ogunsolu
- * Desc: Header file containing all the function prototypes
- *	 and data structures used by the 3-main.c progra
+ * Desc: Header file containing all structures and
+ *       prototypes used by the 3-main.c program.
  */
 
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ * struct op - A struct op.
+ * @op: The operator.
+ * @f: The associated function.
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+	char *op;
+	int (*f)(int a, int b);
 } op_t;
 
 int op_add(int a, int b);
