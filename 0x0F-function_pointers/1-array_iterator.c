3344f4ade5f0c2a26024e8fd21883357b56de82a
Musakalamz <ogunsolumusa@gmail.com>
2023-08-15 23:49:33 -0700

    If you spend too much time thinking about a thing, you'll never get it done
    a function that executes a function given as a parameter on each element of an array.

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,25 @@
+/*
+ * File: 1-array_iterator.c
+ * Auth: Musa_kalamz A Ogunsolu
+ */
+
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Executes a function given as a
+ *                  parameter on each element of an array.
+ * @array: The array.
+ * @size: The size of array.
+ * @action: A pointer to the function to be executed.
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int index;
+
+	if (array == NULL || action == NULL)
+		return;
+
+	for (index = 0; index < size; index++)
+		action(array[index]);
+}
